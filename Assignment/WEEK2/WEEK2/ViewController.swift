e95ecc88a6cdde09553faffce8240ed1781df7f8
sery270 <sery270@naver.com>
2020-10-30 04:13:21 +0900

    [Implement Features] 기본과제  : 스크롤 뷰 속 스택1 완성

---
+++ Assignment/WEEK2/WEEK2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WEEK2
+//
+//  Created by 박세란 on 2020/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
